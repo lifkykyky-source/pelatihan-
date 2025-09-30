4a90a06baab3833f09c77b8f276261c6b00e3a2e
lifkykyky-source <lifkykyky@gmail.com>
2025-09-30 14:12:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,10 @@
     <a href="...">Kunjungi Instragram Saya</a>
     <button id="sapsButton">Sapa Saya!</button>
 </div>
+//1.Pilih tombol berdasarkan ID-nya
+const tombolSapa = document.getElementByld('sapaButton')
+//2.Tambahkan 'event listerner' untuk aksi 'click'
+tombolSapa.addEventListener('click',function(){
+    //3.Jalankan kode ini ketika tombol di-klik
+    alert('Halo! Terima kasih sudah berkunjung!');
+});
