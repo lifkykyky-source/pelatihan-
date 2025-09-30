e282258a109461ea8966380f31d9b05909641487
lifkykyky-source <lifkykyky@gmail.com>
2025-09-30 13:57:21 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+<div class="kartu-profil">
+    <img src="foto-profil.jpg" ait="foto profil saya">
+    <h1>Nama Lengkap Anda</h1>
+    <a href="...">Kunjungi Instragram Saya</a>
+    <button id="sapsButton">Sapa Saya!</button>
+</div>
